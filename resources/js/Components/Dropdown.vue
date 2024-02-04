c0dfbbc229a8fcfdededbfa87e2ef7be77d50e30
Sale2021 <salediallo61@gmail.com>
2024-02-04 04:28:38 +0000

    add backoffice view structure , auth

---
+++ resources/js/Components/Dropdown.vue
@@ -0,0 +1,10 @@
+<template>
+    <li class="menu-item has-submenu">
+        <a class="menu-link" href="">
+            <slot name="nom"></slot>
+        </a>
+        <div class="submenu">
+            <slot />
+        </div>
+    </li>
+</template>
